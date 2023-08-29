df67c831421f30109682bce9b8309ec9eb619c75
unknown <hallysust@gmail.com>
2023-08-29 21:40:27 +0600

    live link added

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+
+### [Live Link:](https://next-module-13-assignment.vercel.app/)
+
+### [Repo Link:](https://github.com/hasankarim18/next-module-13-assignment)
--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
             <hr/>
 
             <HeaderModification />
-            
+            <footer className="my-12"></footer>
         </div>
     );
 };
